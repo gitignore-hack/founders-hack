f91e6e2c3403c69f902cc11589a893e1020ee968
gitignore-hack <gitignore.hack@gmail.com>
2025-05-25 22:53:59 +1000

    Delete frontend directory

--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
-      <h1 className="text-3xl font-inter font-medium">Let's win the hack!</h1>
-    </div>
-  );
-}
